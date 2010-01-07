01a9eea6eb0b47c6b7f793ebd77c434c57db1f71
Anatoliy <be@webdesk.(none)>
2010-01-08 01:21:34 +0300

    Base editor

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,6 +1,8 @@
 /*global jQuery*/
 // http://xpoint.ru/know-how/WYSIWYG/TrueJavaScriptEditor
 // http://www.mozilla.org/editor/midasdemo/
+// http://www.quirksmode.org/dom/execCommand.html
+// 15-17 x 21-24
 (function ($) {
 
 	var undef;
@@ -21,26 +23,118 @@
 
 			var control_panel = document.createElement('div');
 			control_panel.style.width = this.textarea.offsetWidth + 'px';
-			control_panel.style.height = '20px';
-			control_panel.style.backgrounColor = '#eeeeee';
+			control_panel.style.height = '22px';
+			control_panel.style.background = 'ThreeDFace';
 			div.appendChild(control_panel);
 
 			var iframe = document.createElement('iframe');
 			iframe.setAttribute('frameborder', 0);
 			iframe.src = '#';
+			iframe.style.display = 'none';
 			iframe.style.width = this.textarea.offsetWidth + 'px';
-			iframe.style.height = this.textarea.offsetHeight - 30 + 'px';
+			iframe.style.height = this.textarea.offsetHeight - 32 + 'px';
 			div.appendChild(iframe);
+
+			var style = document.createElement('link');
+			style.rel = 'stylesheet';
+			style.type = 'text/css';
+			style.href = 'common.css';
+			iframe.contentWindow.document.getElementsByTagName('head')[0].appendChild(style);
+
 
 			var resizer = document.createElement('div');
 			resizer.style.width = this.textarea.offsetWidth + 'px';
-			resizer.style.height = '20px';
-			resizer.style.backgrounColor = '#eeeeee';
+			resizer.style.height = '6px';
+			resizer.style.backgroundColor = '#eeeeee';
 			resizer.style.cursor = 's-resize';
 			div.appendChild(resizer);
 
 			this.iframe = iframe;
 			this.editor_block = div;
+			this.control_panel = control_panel;
+			this.resizer = resizer;
+			this.init_controls();
+		},
+		init_controls: function () {
+			var editor = this.iframe.contentWindow.document;
+			var commands = [
+				{
+					image: 'text_bold',
+					action: 'bold'
+				},
+				{
+					image: 'text_italic',
+					action: 'italic'
+				},
+				{
+					image: 'text_underline',
+					action: 'underline'
+				},
+				{
+					image: 'text_align_left',
+					action: 'justifyleft'
+				},
+				{
+					image: 'text_align_center',
+					action: 'justifycenter'
+				},
+				{
+					image: 'text_align_right',
+					action: 'justifyright'
+				},
+				{
+					image: 'text_align_justify',
+					action: 'justifyfull'
+				},
+				{
+					image: 'arrow_undo',
+					action: 'undo'
+				},
+				{
+					image: 'arrow_redo',
+					action: 'redo'
+				}
+			];
+			for (var i = 0, len = commands.length; i < len; i++) {
+				var button = document.createElement('div');
+				var cmd = commands[i];
+				//button.style.border = '1px solid';
+				button.style.width = '16px';
+				button.style.height = '16px';
+				button.style.margin = '2px';
+				button.style.display = 'inline-block';
+				button.style.textAlign = 'center';
+				button.style.cursor = 'default';
+				button.style.background = 'ThreeDFace url(images/' + cmd.image + '.png)';
+				button.onmouseover = (function (b) {
+					return function () {
+						//b.style.backgroundColor = 'ThreeDHighlight';
+					};
+				})(button);
+				button.onmouseout = (function (b) {
+					return function () {
+						//b.style.backgroundColor = 'ThreeDFace';
+						b.style.backgroundPosition = '0 0';
+					};
+				})(button);
+				button.onmousedown = (function (b) {
+					return function () {
+						//b.style.backgroundColor = '#eee';
+						b.style.backgroundPosition = '1px 1px';
+						return false;
+					};
+				})(button);
+				button.onmouseup = (function(cmd, b) {
+					return function () {
+						editor.execCommand(cmd.action, null, '');
+						//b.style.backgroundColor = 'ThreeDFace';
+						b.style.backgroundPosition = '0 0';
+						return false;
+					};
+				})(cmd, button);
+				this.control_panel.appendChild(button);
+			}
+			
 		},
 		get_iframe: function () {
 			if (this.iframe === undef) {
@@ -51,7 +145,6 @@
 		export_textarea: function () {
 			var w = this;
 			try {
-				console.log(w.iframe, w.iframe.contentWindow, w.iframe.contentWindow.document, w.iframe.contentWindow.document.body);
 				w.iframe.contentWindow.document.body.innerHTML = w.textarea.value;
 				w.iframe.contentWindow.document.designMode = 'on';
 				w.iframe.focus();
